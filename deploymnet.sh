786da8fd14ae247c84ce127b0e733e1729bd4186
hilaire hameni <linuxclass102022@gmail.com>
2023-02-24 16:29:55 -0600

    modifying deploymnet.sh

--- deploymnet.sh
+++ deploymnet.sh
@@ -7,3 +7,5 @@ touch linuxjobs
 touch jira10
 touch file1
 echo "hello welcome to linux class and ready for big check"
+echo " Be ready, nothing comes easy but by perseverance we can archive big dreams"
+touch file3
