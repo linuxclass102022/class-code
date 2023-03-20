7f9c5970186e6b006cc1852a97a244a41ecf29aa
hilaire hameni <linuxclass102022.com>
2023-03-20 16:06:39 -0400

    modifying deployment.sh

--- deploymnet.sh
+++ deploymnet.sh
@@ -10,3 +10,4 @@ echo "hello welcome to linux class and ready for big check"
 echo " Be ready, nothing comes easy but by perseverance we can archive big dreams"
 touch file3
 touch file4
+touch file5
